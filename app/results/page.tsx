d413c9e001ef35bae4525d7e676935bd620b92b9
vshital <shitalvhanamane@gmail.com>
2026-04-02 10:27:32 +0530

    Fix build

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { translations } from "@/lib/lang";
